960a50aa1f3b90bec9e4ed3bb0ee59015fe2cbb5
brayhanjc <brayhanjaramillo@hotmail.com>
2018-05-06 13:48:36 -0500

    Agregnado interfaz de usuario y agregando validacion en el login para entrar a una de las dos interfaces

--- BienestarUq/app/src/main/java/ds/bienestaruq/Login.kt
+++ BienestarUq/app/src/main/java/ds/bienestaruq/Login.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class Login : AppCompatActivity() , View.OnClickListener{
 
@@ -12,9 +14,14 @@ class Login : AppCompatActivity() , View.OnClickListener{
 
     }
 
+
+    lateinit var editTextName:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 
     /***
@@ -22,8 +29,18 @@ class Login : AppCompatActivity() , View.OnClickListener{
      */
     fun  ventanaPrincipal(view: View){
 
-        val intent = Intent(this, Principal::class.java)
-        startActivity(intent)
+        editTextName =findViewById(R.id.txtUsuario) as EditText
+        val name=editTextName.text.toString()
+
+
+        if (name.equals("1")){
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        }
+        if (name.equals("2")){
+            val intent = Intent(this, Usuario::class.java)
+            startActivity(intent)
+        }
 
     }
 }
