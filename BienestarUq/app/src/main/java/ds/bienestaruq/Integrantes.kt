38b727adbbc60653baaf35bcb54a27bb0a27887a
brayhanjc <brayhanjaramillo@hotmail.com>
2018-05-07 12:40:59 -0500

    agregando icono aplicacion y editndo interfaz usuario

---
+++ BienestarUq/app/src/main/java/ds/bienestaruq/Integrantes.kt
@@ -0,0 +1,12 @@
+package ds.bienestaruq
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Integrantes : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_integrantes)
+    }
+}
